309ed399cbdd24dd5d4decce0af45e325b9402bd
DrumPire <expresproject@gmail.com>
2021-11-21 19:00:26 +0200

    add search and goods page

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,23 +1,78 @@
 const getGoods = () => {
-  const links = document.querySelectorAll('.navigation-link');
+  const links = document.querySelectorAll('.navigation-link'),
+        viewAllBtn = document.querySelector('.more');
   
+  const renderGoods = goods => {
+    const goodsContainer = document.querySelector('.long-goods-list');
+    
+    goodsContainer.innerHTML ='';
 
-  const getData = () => {
+    goods.forEach(good => {
+      const goodBlock = document.createElement('div');
+
+      
+
+      goodBlock.classList.add('col-lg-3');
+      goodBlock.classList.add('col-sm-6');
+
+      goodBlock.innerHTML = `
+        <div class="goods-card">
+          <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+          <img src="db/${good.img}" alt="${good.name}" class="goods-image">
+          <h3 class="goods-title">${good.name}</h3>
+          <p class="goods-description">${good.description}</p>
+          <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+            <span class="button-price">$${good.price}</span>
+          </button>
+        </div>
+      `;
+
+      goodsContainer.append(goodBlock);
+
+    });
+
+  };
+
+  const getData = (value, category) => {
     fetch('https://test-ec323-default-rtdb.europe-west1.firebasedatabase.app/db.json')
       .then(res => res.json())
       .then(data => {
-        localStorage.setItem('goods', JSON.stringify(data));
-        const goods = JSON.parse(localStorage.getItem('goods'));
-        console.log(goods);
+        const array =  category ? data.filter(item => item[category] === value) : data;
+
+        if (window.location.pathname !== '/goods.html') {
+          window.location.href = '/goods.html';
+        } else {
+          renderGoods(array);
+        }
+        
+        localStorage.setItem('goods', JSON.stringify(array));
       });
   };
 
   links.forEach(link => {
     link.addEventListener('click', e => {
       e.preventDefault();
+      const linkValue = link.textContent;
+      const category = link.dataset.field;
+
+      getData(linkValue, category);
+    });
+
+  });
+
+  try {
+    viewAllBtn.addEventListener('click', e => {
+      e.preventDefault();
+  
       getData();
     });
-  });
+  } catch (e) {
+    console.log(e);
+  }
+
+  if (localStorage.getItem('goods') && window.location.pathname === '/goods.html') {
+    renderGoods(JSON.parse(localStorage.getItem('goods')));
+  }
 
 };
 
